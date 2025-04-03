1767308ff3f9741aec182db3edf0ef50d35a6e5c
Kush <150859750+kushptl1@users.noreply.github.com>
2025-04-03 12:45:53 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-kp"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-00a929b66ed6e0de6"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Terraform-Test-Instance"
+  }
+}
+
